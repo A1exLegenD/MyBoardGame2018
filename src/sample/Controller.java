983b4e774509489b2cbaaabea3caf7741975bbf9
A1exLegenD <alex53291@hotmail.com>
2018-12-05 12:22:21 +0100

    commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,43 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+public class Controller {
+
+    @FXML
+    private Canvas backg, foreg;
+
+
+    @FXML
+    private void init(){
+        Square s = new Square(50,Color.BLACK,100,100);
+        drawSquare(s);
+
+    }
+    @FXML
+    private void init2(){
+        Square s = new Square(50,Color.WHITE,200,100);
+        drawSquare(s);
+
+    }
+    /**
+     * Draws a single square on the background
+     * @param s the square to be drawn
+     */
+    private void drawSquare(Square s)
+    {
+        GraphicsContext gc = backg.getGraphicsContext2D();
+        gc.setFill(s.getColor());
+        gc.fillRect(s.getPosX(), s.getPosY(), s.getSize(), s.getSize());
+    }
+
+
+}
